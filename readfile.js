f5d106138deaa6d8809e2632bab3da2553aed4b5
Hoang-Minh <quataoxanh83@gmail.com>
2018-04-29 16:28:53 -0700

    read file and output each word by a comma

--- readfile.js
+++ readfile.js
@@ -1,5 +1,17 @@
 var fs = require("fs");
-fs.readFile("movies.txt", "utf-8", function(err, data){
-    if(err) throw err;
-    console.log(data);
-});
+//var fileName = "movies.txt";
+
+function readAFile(fileName){
+    fs.readFile(fileName, "utf-8", function(err, data){
+        if(err) throw err;
+        var arr = data.split(",");
+        console.log("file: " + fileName);
+        
+        for(var i = 0; i < arr.length; i++){
+            console.log(arr[i]);
+        }
+    });    
+}
+
+readAFile("movies.txt");
+readAFile("best_thing_ever.txt");
